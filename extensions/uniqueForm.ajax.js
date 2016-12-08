a298a0e8d245d78ddf59ceaf1696241d042c19be
Vít Kutný <vitkutny@icloud.com>
2016-12-08 19:17:57 +0100

    Extension: uniqueForm.ajax.js
    - unikatnost i pro neajaxove/napul ajaxove formulare

--- extensions/uniqueForm.ajax.js
+++ extensions/uniqueForm.ajax.js
@@ -1,33 +1,59 @@
-(function ($) {
+(function ($, document) {
 	/**
 	 * @author Vít Kutný
 	 *
 	 * Unique form
-	 *  - zamezí duplicitnímu odeslání ajaxového formuláře
+	 *  - zamezí duplicitnímu odeslání formuláře
 	 *   - dvojklik
 	 *   - dlouhotrvající request, kdy uživatel zkusí formulář odeslat znovu
 	 *   - pokud je navěšené odeslání po změně hodnoty inputu, tak po změně a potvrzení enterem dochází jak k odeslání formuláře enterem a zároveň přes event
 	 */
 	$.nette.ext('uniqueForm', {
+		init: function () {
+			var uniqueForm = this;
+			$(document).on('submit', 'form', function (event) {
+				var $form = $(this);
+				uniqueForm.formSubmitBeforeHandler.apply(uniqueForm, [$form]);
+				setTimeout(function () {
+					//disable je třeba nastavit opožděně, aby formulář odeslal v POST requestu i submit button
+					$form.find(uniqueForm.inputSubmitSelector).each(uniqueForm.inputSubmitBeforeHandler);
+				}, 0); //setTimeout() re-queues the new JavaScript at the end of the execution queue
+				$form.data('uniqueFormTimeout', setTimeout(function () {
+					uniqueForm.formSubmitAfterHandler.apply(uniqueForm, [$form]);
+					$form.find(uniqueForm.inputSubmitSelector).each(uniqueForm.inputSubmitAfterHandler)
+				}, uniqueForm.timeout));
+			});
+		},
 		start: function (xhr, settings) {
 			if ('nette' in settings && 'form' in settings.nette && settings.nette.form) {
-				settings.nette.form
-					.bind('submit', this.formSubmitHandler)
-					.find(this.inputSubmitSelector).each(this.inputSubmitBeforeHandler)
-				;
+				var $form = settings.nette.form;
+				this.formSubmitBeforeHandler.apply(this, [$form]);
+				$form.find(this.inputSubmitSelector).each(this.inputSubmitBeforeHandler);
 			}
 		},
 		complete: function (xhr, status, settings) {
 			if ('nette' in settings && 'form' in settings.nette && settings.nette.form) {
-				settings.nette.form
-					.unbind('submit', this.formSubmitHandler)
-					.find(this.inputSubmitSelector).each(this.inputSubmitAfterHandler)
-				;
+				var $form = settings.nette.form;
+				this.formSubmitAfterHandler.apply(this, [$form]);
+				$form.find(this.inputSubmitSelector).each(this.inputSubmitAfterHandler)
 			}
 		}
 	}, {
+		timeout: 5000,
 		formSubmitHandler: function () {
 			return false;
+		},
+		formSubmitBeforeHandler: function ($form) {
+			clearTimeout($form.data('uniqueFormTimeout'));
+			$form.removeData('uniqueFormTimeout')
+				.bind('submit', this.formSubmitHandler)
+			;
+		},
+		formSubmitAfterHandler: function ($form) {
+			clearTimeout($form.data('uniqueFormTimeout'));
+			$form.removeData('uniqueFormTimeout')
+				.unbind('submit', this.formSubmitHandler)
+			;
 		},
 		inputSubmitSelector: 'input[type=submit], button[type=submit], input[type=image]',
 		inputSubmitBeforeHandler: function () {
@@ -47,4 +73,4 @@
 			}
 		}
 	});
-})(jQuery);
+})(window.jQuery, window.document);
