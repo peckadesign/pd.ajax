0a34ecfff4e44e72b55488936c084578c8059a52
Radek Šerý <radek.sery@peckadesign.cz>
2017-08-17 15:21:05 +0200

    #4 Připravit kompatibilní verzi pro pdBox 1.1.0
    - příprava pro kompatibility s peckadesign/jquery.pdbox ve verzi 1.1 a vyšší

--- extensions/pdbox.ajax.js
+++ extensions/pdbox.ajax.js
@@ -5,10 +5,10 @@
 	 * @author Jiří Pudil
 	 *
 	 * Extension pro ajaxový pdbox, včetně historie (volitelně). Pro použití stačí pro ovládací element (tj. ten
-	 * s class thickbox) přidat class ajax, pak např. data-spinner=".pd-box-content" atd., další extension by měla být
-	 * kompatibilní :)
+	 * s class js-pdbox např.) přidat class ajax, pak např. data-no-spinner atd., další extension by měly
+	 * být kompatibilní :)
 	 *
-	 * @todo Pokud otevřu TB, zavřu TB, zafiltruji, otevřu nový TB a pak v historii přejdu přímo na ten 1. otevřený (tj. přeskočím několik mezikroků), pak po zavření TB budu mít URL nekonzistentní vůči obsahu. Nevím, zda je to vůbec řešitelné, respektive jak snadno/těžko.
+	 * @todo Pokud otevřu pdbox, zavřu pdbox, zafiltruji, otevřu nový pdbox a pak v historii přejdu přímo na ten 1. otevřený (tj. přeskočím několik mezikroků), pak po zavření pdbox budu mít URL nekonzistentní vůči obsahu. Nevím, zda je to vůbec řešitelné, respektive jak snadno/těžko.
 	 *
 	 * @todo Nemělo by v lastState a original být též uloženo ui?
 	 */
@@ -33,7 +33,7 @@
 
 					// při popstate kontrolujeme, zda nový stav má nastavenou vlastnost pdbox, pokud ano, otevřeme jej (metoda open kontroluje, zda již otevřený není), vložíme obsah a spustíme událost load
 					if ('pdbox' in state && state.pdbox) {
-						this.historyEnabled = true; // protože jde o popstate, jde o TB s historií (pokud je to TB, není třeba řešit tady), tj. nastavíme historyEnabled na true, aby po zavření křížkem došlo k pushState
+						this.historyEnabled = true; // protože jde o popstate, jde o pdbox s historií (pokud je to pdbox, není třeba řešit tady), tj. nastavíme historyEnabled na true, aby po zavření křížkem došlo k pushState
 
 						this.box.open();
 						this.box.content(state.pdbox.content);
@@ -42,12 +42,12 @@
 					} else {
 						this.historyEnabled = false; // zavíráme pomocí tlačítka zpět/vpřed, tj. nechceme zapisovat do historie
 
-						// pokud pdbox není, zavřeme jej (metoda close opět kontroluje, zda je TB otevřený)
+						// pokud pdbox není, zavřeme jej (metoda close opět kontroluje, zda je pdbox otevřený)
 						this.box.close();
 					}
 
 					// udržujeme si aktuální stav, díky němu víme, kam se vracet (viz metoda open a push do original) i v případě popstate
-					// upravit až po otevření TB (pokud se otevíral, není třeba kontrolovat)
+					// upravit až po otevření pdbox (pokud se otevíral, není třeba kontrolovat)
 					this.lastState = {
 						location: location.href,
 						state: state,
@@ -56,8 +56,8 @@
 				}, this));
 
 				if (this.box) {
-					this.box.addEventListener('open', $.proxy(function () {
-						// po otevření TB vždy uložíme kontext, který je pod TB, aby bylo možné po zavření vše vrátit zpět
+					this.box.addEventListener('beforeOpen', $.proxy(function () {
+						// při otevření pdbox vždy uložíme kontext, který je pod pdbox, aby bylo možné po zavření vše vrátit zpět
 						if (this.popstate) {
 							this.original.push(this.lastState);
 						}
@@ -91,9 +91,9 @@
 				title: document.title
 			};
 
-			// pokud success nastal po kliknutí na elementu s class thickbox, vyvoláme load událost, nastavíme vlastnosti TB a pokud je povolená historie, nahradíme stávající stav naším, kde přidáváme do state vlastnost pdbox
-			if (('nette' in settings && 'el' in settings.nette && settings.nette.el.is('.thickbox')) || settings.pdbox) {
-				// zpracování vráceného redirectu v rámci TB
+			// pokud success nastal po kliknutí na elementu s class this.pdboxSelector, vyvoláme load událost, nastavíme vlastnosti pdbox a pokud je povolená historie, nahradíme stávající stav naším, kde přidáváme do state vlastnost pdbox
+			if (('nette' in settings && 'el' in settings.nette && settings.nette.el.is(this.pdboxSelector)) || settings.pdbox) {
+				// zpracování vráceného redirectu v rámci pdbox
 				if (payload.redirect) {
 					var options = {
 						url: payload.redirect,
@@ -116,7 +116,7 @@
 					return;
 				}
 
-				this.box.window.content.find(this.ajaxified).addClass('thickbox');
+				this.box.window.content.find(this.ajaxified).addClass(this.pdboxAutoClass); // .slice(1) odstraní z názvu class
 				this.box.dispatchEvent('load', {content: payload});
 
 				if ($.inArray('history', settings.off) === -1 && this.historySupported) {
@@ -136,16 +136,18 @@
 			}
 		}
 	}, {
+		pdboxSelector: '.js-pdbox',
+		pdboxAutoClass: 'js-pdbox',  // jde o název class, ne selector! automaticky nastavovaná třída ajax odkazům uvnitř otevřeného pdboxu; na tuto třídu by měl být pdbox též inicializován
 		historySupported: false, // nastavujeme v init
-		historyEnabled: false, // zabrání/povolí pushState po zavření okna TB; jdeme-li zpět (popstate), tak při zavření TB nechceme vkládat nový stav do historie, dále brání vložení stavu pro TB bez historie (např. obrázkový TB)
+		historyEnabled: false, // zabrání/povolí pushState po zavření okna pdbox; jdeme-li zpět (popstate), tak při zavření pdbox nechceme vkládat nový stav do historie, dále brání vložení stavu pro pdbox bez historie (např. obrázkový pdbox)
 		popstate: false,
 		pushOriginal: function () {
-			// fce pro vložení stavu do historie, volá se po zavření TB; nový stav vkládá pouze pokud má co vložit (tj. víme co je pod TB) a pokud nezavíráme TB tlačítkem zpět
-			// pop stavu se vyvolá vždy při zavření TB, pushState ale chceme jen pokud není zavření vyvoláno popstatem
+			// fce pro vložení stavu do historie, volá se po zavření pdbox; nový stav vkládá pouze pokud má co vložit (tj. víme co je pod pdbox) a pokud nezavíráme pdbox tlačítkem zpět
+			// pop stavu se vyvolá vždy při zavření pdbox, pushState ale chceme jen pokud není zavření vyvoláno popstatem
 			if (this.historyEnabled) {
 				var state = this.original.pop();
 				if (state) {
-					if (history.state === undefined || (history.state.href != state.location)) { // zavření TB tlačítkem zpět pop je stejný stav, jako aktuální a nechceme jej zduplikovat
+					if (history.state === undefined || (history.state.href != state.location)) { // zavření pdbox tlačítkem zpět pop je stejný stav, jako aktuální a nechceme jej zduplikovat
 						history.pushState(state.state, state.title, state.location);
 						document.title = state.title;
 
@@ -162,7 +164,7 @@
 			}
 		},
 		ajaxified: '', // selector všeho, co by mělo být odesíláno ajaxem
-		original: [], // zásobník stavů "pod TB" - po zavření TB musíme nastavit URL, title a stav do historie přes pushState
+		original: [], // zásobník stavů "pod pdbox" - po zavření pdbox musíme nastavit URL, title a stav do historie přes pushState
 		lastState: null,
 		box: null
 	});
